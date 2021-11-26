298d5380bc20ce7b5676dd9ff240d5020c4cb408
GrzegorzKasprzyk <xankrigor87@gmail.com>
2021-11-26 11:06:36 +0100

    changes file

--- Calculator.WindowsFormsApp/Form1.cs
+++ Calculator.WindowsFormsApp/Form1.cs
@@ -16,5 +16,10 @@ namespace Calculator.WindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
